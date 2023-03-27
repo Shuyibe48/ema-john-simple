2a3b68f89ef5c3f755ed73bfef5504976ce89940
Shuyibe48 <gthrough48@gmail.com>
2023-03-28 00:39:50 +0600

    product and cart show on ui

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Products.css'
+
+const Products = (props) => {
+    const { name, img, price, seller, ratings } = props.product
+    const cartInfo = props.cartInfo
+
+    return (
+        <div>
+            <div className='card'>
+                <div className='img'>
+                    <img src={img} />
+                </div>
+                <div className='card-body'>
+                    <h6>{name}</h6>
+                    <p className='price'>Price: ${price}</p>
+                    <div className='card-optional-info'>
+                        <p className='seller'>Manufacturer : {seller}</p>
+                        <p className='ratings'>Ratings : {ratings} star</p>
+                    </div>
+                </div>
+                <div className='card-footer'>
+                    <button onClick={() => cartInfo(props.product)}>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    const cartInfo = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+
     return (
-        <div className='shop'>
-            <div>
-                <h1>product</h1>
+        <div className='shop-container'>
+            <div className='shop'>
+                {
+                    products.map(product => <Products
+                        product={product}
+                        key={product.id}
+                        cartInfo={cartInfo}
+                    ></Products>)
+                }
             </div>
-            <div>
-                <h1>cart</h1>
+            <div className='cart-container'>
+                <h5>Order Summary</h5>
+                <p>Selected Item: {cart.length}</p>
+                <p>Total Price: </p>
+                <p>Total Shipping Charge: </p>
+                <p>Tax: </p>
+                <p>Grand Total: </p>
+                <button>Clear Chat</button>
+                <button>Review Order</button>
             </div>
         </div>
     );
